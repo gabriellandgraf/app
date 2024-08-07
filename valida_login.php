3e6ce214b858dc8694655ad29f24d17b616e1c5e
gabriellandgraf <gabriellandgrafdecarvalho54@gmail.com>
2024-08-07 17:12:19 -0300

    Add files via upload

---
+++ valida_login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>App Help Desk</title>
+</head>
+
+
+<body>
+
+    <?php
+/*Professora por favor faça uma oração antes de começar e se pór acaso ficar confusa não responsabilizo pois não sei explicar desde agradeço !*/
+
+    /*$usuario_autenticator=false;  É uma função para criar a condição! */
+
+     /*   $usuarios_app=array É a criação de uma array! */
+           /* array( Criar um array dentro de outra array!*/
+                'email' => 'landgrafcarvalho@outlook.com',
+                'senha' => 'zbzhn245'
+            ),
+            array(
+                'email' => 'landgrafcarvalho@outlook.com',
+                'senha' => 'zbzhn2456'
+            )
+        );
+
+       /* foreach ($usuarios_app as $user) { O foreach serve para ver se os arrays acima são verdadeiros! */
+           /* if ($user['email']==$_POST['email'] && $user['senha']==$_POST['senha']) {
+                $usuario_autenticator=true;
+            }
+        } Esse if é para buscar os email no servidor! */
+
+       /* if($usuario_autenticator) {
+            echo "Usuário Autenticado"; Esse if é para autenticar se o usuario existe! 
+        }Esse if é para autenticar se o usuario existe!*/ /* else {
+            echo "Usuário não encontrado!";
+        } Se o usuário nãe existir vai dar não encontrado! */
+
+
+
+
+    ?>
+
+</body>
+
+</html>
